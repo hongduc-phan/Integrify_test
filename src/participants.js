eecf154d168f93797d2eb604532a2be8cee5696e
UbuntuAsus <phduc.phanhongduc@gmail.com>
2018-03-17 13:59:31 +0200

    upöoad file js participants.js

---
+++ src/participants.js
@@ -0,0 +1,123 @@
+[
+  {
+    id: data.get('a123456aa1'),
+    name: data.get('DaveGreat'),
+    email: data.get('dave@gmail.com'),
+    tel: data.get('123456789'),
+  },
+  {
+    id: data.get('a123456ab2'),
+    name: data.get('DavidMoore'),
+    email: data.get('davidmoore@gmail.com'),
+    tel: data.get('987654321'),
+  },
+  {
+    id: data.get('a123456ab3'),
+    name: data.get('MichelRues'),
+    email: data.get('michelRues@gmail.com'),
+    tel: data.get('987655321'),
+  },
+  {
+    id: data.get('a123456ab4'),
+    name: data.get('DavidBeckham'),
+    email: data.get('davidbeckham@gmail.com'),
+    tel: data.get('987634321'),
+  },
+  {
+    id: data.get('a123456ab5'),
+    name: data.get('CrisRonaldo'),
+    email: data.get('cronaldo@gmail.com'),
+    tel: data.get('987154321'),
+  },
+  {
+    id: data.get('a123456ab6');
+    name: data.get('TonyAllen'),
+    email: data.get('toniallen@gmail.com'),
+    tel: data.get('987254321'),
+  },
+  {
+    id: data.get('a123456ab7'),
+    name: data.get('ToniKross'),
+    email: data.get('tonikross@gmail.com'),
+    tel: data.get('987657329'),
+  },
+  {
+    id: data.get('a123456ab8'),
+    name: data.get('ManuerNueul'),
+    email: data.get('manueul@gmail.com'),
+    tel: data.get('987354328'),
+  },
+  {
+    id: data.get('a123456ab9'),
+    name: data.get('DiegoForlan'),
+    email: data.get('diegoforlan@gmail.com'),
+    tel: data.get('187654327'),
+  },
+  {
+    id: data.get('a123456ab10'),
+    name: data.get('FrankLampard'),
+    email: data.get('franklampard@gmail.com'),
+    tel: data.get('287654326'),
+  },
+  {
+    id: data.get('a123456ab11'),
+    name: data.get('JoBeoteang'),
+    email: data.get('jobeoteang@gmail.com'),
+    tel: data.get('387654325'),
+  },
+  {
+    id: data.get('a123456ab12'),
+    name: data.get('MarkHummels'),
+    email: data.get('markhummels@gmail.com'),
+    tel: data.get('487654323'),
+  },
+  {
+    id: data.get('a123456ab13'),
+    name: data.get('PhillipLahm'),
+    email: data.get('philliplahm@gmail.com'),
+    tel: data.get('587654321'),
+  },
+  {
+    id: data.get('a123456ab14'),
+    name: data.get('OliverKahn'),
+    email: data.get('oliverkahn@gmail.com'),
+    tel: data.get('687654321'),
+  },
+  {
+    id: data.get('a123456ab15'),
+    name: data.get('ArienRobben'),
+    email: data.get('arienrobben@gmail.com'),
+    tel: data.get('787654321'),
+  },
+  {
+    id: data.get('a123456ab16'),
+    name: data.get('FrankRiberry'),
+    email: data.get('frankriberry@gmail.com'),
+    tel: data.get('887654321'),
+  },
+  {
+    id: data.get('a123456ab17'),
+    name: data.get('AcantaThiego'),
+    email: data.get('acantathiego@gmail.com'),
+    tel: data.get('917654321'),
+  },
+  {
+    id: data.get('a123456ab18'),
+    name: data.get('DavidAlaba'),
+    email: data.get('davidalaba@gmail.com'),
+    tel: data.get('927654321'),
+  },
+  {
+    id: data.get('a123456ab19'),
+    name: data.get('JamesRogrerez'),
+    email: data.get('jamesrogrerez@gmail.com'),
+    tel: data.get('937654321'),
+  },
+  {
+    id: data.get('a123456ab20'),
+    name: data.get('MarioGotze'),
+    email: data.get('mariogotze@gmail.com'),
+    tel: data.get('947654321'),
+  },
+  
+]
